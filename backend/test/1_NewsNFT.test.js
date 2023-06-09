1aace2492d68d3e8930743a18a67388a3e025f44
Till Grutschus <till.grutschus@outlook.com>
2023-06-09 14:06:58 +0000

    finished verificationState

--- backend/test/1_NewsNFT.test.js
+++ backend/test/1_NewsNFT.test.js
@@ -1,46 +1,54 @@
 const truffleAssert = require("truffle-assertions");
 const NewsNFT = artifacts.require("NewsNFT");
 
+/**
+ * @dev Here we are testing functionality related to minted NFTS.
+ * This includes minting itself, transferring, voting, and
+ * verification of the minted token. 
+ * Testing of the NewsNFT contract itself, i.e. ownerhsip of the contract,
+ * permissions, is done in Solidity.
+ */
 contract("NewsNFT", (accounts) => {
-    it("anyone should be able to mint NFTs", async () => {
-        const newsNFT = await NewsNFT.deployed();
-        const owner = accounts[0];
+    const alice = accounts[1];
+    const bob = accounts[2];
+    let newsNFT;
+    let contractOwner;
+    let aliceTokenID;
+    let bobTokenID;
 
-        const transaction = await newsNFT.createNewsItem("test", { from: owner });
-        const logs = transaction.logs;
-        const tokenId = logs[0].args.tokenId.toNumber();
-
-
-        assert.equal(tokenId, 0, "tokenId should be 0");
+    beforeEach(async function () {
+        newsNFT = await NewsNFT.deployed();
+        contractOwner = await newsNFT.owner();
+        aliceTokenID = (await newsNFT.createNewsItem("alices news", { from: alice })).logs[0].args.tokenId.toNumber();
+        bobTokenID = (await newsNFT.createNewsItem("bobs news", { from: bob })).logs[0].args.tokenId.toNumber();
     });
 
     it("minter should be owner of NFTs", async () => {
-        const newsNFT = await NewsNFT.deployed();
-        const owner = accounts[0];
-
-        // mint NFT
-        await newsNFT.createNewsItem("test", { from: owner });
-
-        // check owner
-        const tokenOwner = await newsNFT.ownerOf(0);
-
-        assert.equal(tokenOwner, owner, "owner should be minter");
+        const tokenOwner = await newsNFT.ownerOf(aliceTokenID);
+        assert.equal(tokenOwner, alice, "owner should be minter");
     });
 
-    it("owner cannot transfer NFTs", async () => {
-        const newsNFT = await NewsNFT.deployed();
-        const owner = accounts[0];
-        const receiver = accounts[1];
-
-        // mint NFT
-        await newsNFT.createNewsItem("test", { from: owner });
-
+    it("token owner cannot transfer NFTs", async () => {
         // transfer NFT
         await truffleAssert.reverts(
-            newsNFT.safeTransferFrom(owner, receiver, 0, { from: owner }),
+            newsNFT.safeTransferFrom(alice, bob, aliceTokenID, { from: alice }),
             "NewsNFT: Token transfers are not allowed"
         );
-
     });
 
+    it("verification state should default to unverified", async () => {
+        const state = await newsNFT.getVerificationState(aliceTokenID);
+
+        assert.equal(state, 0, "state should be unverified (0)");
+    });
+
+    it("verification state can only be set by contract owner", async () => {
+        await truffleAssert.reverts(
+            newsNFT.setVerificationState(aliceTokenID, 1, { from: alice }),
+            "Ownable: caller is not the owner"
+        );
+        await newsNFT.setVerificationState(aliceTokenID, 1, { from: contractOwner });
+        const state = await newsNFT.getVerificationState(aliceTokenID);
+        assert.equal(state, 1, "state should be verified (1)");
+    });
 })
--- backend/test/1_NewsNFT.test.sol
+++ backend/test/1_NewsNFT.test.sol
@@ -5,82 +5,11 @@ pragma solidity ^0.8.0;
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/NewsNFT.sol";
-import "./NewsNFTOwner.sol";
 
+/**
+ * @dev Here we are testing the NewsNFT contract and its functions.
+ * We test the permission and governance system of the contract.
+ */
 contract TestNewsNFT {
-    NewsNFT newsNFT;
-    NewsNFTOwner owner1;
-    NewsNFTOwner owner2;
-    address add_owner1;
-    address add_owner2;
 
-    function beforeEach() public {
-        newsNFT = NewsNFT(DeployedAddresses.NewsNFT());
-        owner1 = new NewsNFTOwner(newsNFT);
-        add_owner1 = address(owner1);
-        owner2 = new NewsNFTOwner(newsNFT);
-        add_owner2 = address(owner2);
-    }
-
-    /**
-     * Anyone can read the VerificationState of a NewsNFT.
-     * Default must be Unverified.
-     */
-    function testDefaultVerificationState() public {
-        uint256 tokenID = owner1.tokenID();
-
-        uint256 expected = uint256(NewsNFT.VerificationState.Unverified);
-        uint256 actual = uint256(newsNFT.getVerificationState(tokenID));
-        Assert.equal(
-            expected,
-            actual,
-            "NewsNFT should be unverified by default"
-        );
-    }
-
-    // function testSetVerificationState() public {
-    //     NewsNFT newsNFT = new NewsNFT();
-    //     newsNFT.setVerificationState(true);
-    //     Assert.equal(
-    //         newsNFT.getVerificationState(),
-    //         true,
-    //         "NewsNFT should be verified"
-    //     );
-    // }
-
-    // // write a test to check that the verification state of a
-    // // NewsNFT can be set to 0 (unverified)
-    // function testUnsetVerificationState() public {
-    //     NewsNFT newsNFT = new NewsNFT();
-    //     newsNFT.setVerificationState(false);
-    //     Assert.equal(
-    //         newsNFT.getVerificationState(),
-    //         false,
-    //         "NewsNFT should be unverified"
-    //     );
-    // }
-
-    // // write a test to check that the verification state of a
-    // // NewsNFT can be set to 1 (verified)
-    // function testSetVerificationStateWithAddress() public {
-    //     NewsNFT newsNFT = new NewsNFT();
-    //     newsNFT.setVerificationStateWithAddress(address(this), true);
-    //     Assert.equal(
-    //         newsNFT.getVerificationStateWithAddress(address(this)),
-    //         true,
-    //         "NewsNFT should be verified"
-    //     );
-    // }
-
-    // // write a test to check that the verification state of a
-    // // NewsNFT can be set to 0 (unverified)
-    // function testUnsetVerificationStateWithAddress() public {
-    //     NewsNFT newsNFT = new NewsNFT();
-    //     newsNFT.setVerificationStateWithAddress(address(this), false);
-    //     Assert.equal(
-    //         newsNFT.getVerificationStateWithAddress(address(this)),
-    //         false,
-    //         "NewsNFT should be unverified"
-    //     );
-    // }
 }
